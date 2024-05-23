b4858cec196cf1c59ff1980002effa8add2980d9
Phuong Nguyen <phuong.nguyen@trapezegroup.com>
2024-05-23 11:05:49 -0400

    Initial

---
+++ Entities/Report.cs
@@ -0,0 +1,10 @@
+﻿namespace ReportsManager.Entities
+{
+    public class Report
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Services/ReportService.cs
@@ -0,0 +1,22 @@
+﻿using ReportsManager.Entities;
+
+namespace ReportsManager.Services
+{
+    public class ReportService
+    {
+        private List<Report> _reports;
+
+        public ReportService()
+        {
+            _reports = new List<Report>();
+            // TODO: Initialize _reports with actual data
+        }
+
+        public List<Report> SearchReports(string fileName, string description, string content)
+        {
+            return _reports.Where(r => r.Name.Contains(fileName)
+                && r.Description.Contains(description)
+                && r.Content.Contains(content)).ToList();
+        }
+    }
+}
